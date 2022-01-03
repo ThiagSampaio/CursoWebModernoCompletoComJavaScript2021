a1114136c6f623c39d2b0bd783c688fd40d6d65f
Thiago Sampaio <thiagosampaioparticular1@gmail.com>
2022-01-03 15:32:02 -0300

    Organiza comentários da aula 1 e adicona aula 2

---
+++ exercicios-js/1_fundamentos/2_comentario.js
@@ -0,0 +1,15 @@
+// Comentário de uma linha 
+console.log('Linha 1')
+
+/*
+    Comentário de 
+    múltiplas linhas
+*/
+console.log('Linha 2')
+
+/* 
+ * Pricípios de comentários:
+ *
+ * 1-> Evitar comentário desnecessários, ou seja, comentários irrelevantes.
+ * 2-> Escreva um código legível/limpo , ou seja, use nomes que refletem a realidade do negócio. 
+ */
