b4c97c493326ab5d547aba4f892e031167f639b1
Thiago Sampaio <thiagosampaioparticular1@gmail.com>
2022-01-03 16:37:38 -0300

    Impleta aula 4

---
+++ 4_tipagemFraca.js
@@ -0,0 +1,10 @@
+// No Js possui tipo da linguagem dinâmica , ou seja, fracamente tipada.
+// Ou seja, não significa que não possui tipo , porém pode mudar a todo momento o tipo de uma variável. 
+
+let qualquer = 'Legal'
+console.log(qualquer)
+console.log(typeof qualquer)
+
+qualquer = 3.1516
+console.log(qualquer)
+console.log(typeof qualquer)
