c5aa75871140677cf87c4507a0f79a223c886f01
Thiago Sampaio <thiagosampaioparticular1@gmail.com>
2022-01-03 14:21:25 -0300

    first commit

---
+++ exercicios-js/1_fundamentos/1_organizacao.js
@@ -0,0 +1,11 @@
+console.log("Sentença de código")
+
+{
+    {
+        console.log("Olá");
+        console.log('Mundo!') // Padrão do curso
+    }
+}
+
+// 3 Sentenças de código
+// 2 blocos de código
