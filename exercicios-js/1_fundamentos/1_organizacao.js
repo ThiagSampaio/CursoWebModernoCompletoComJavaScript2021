22339b795089d786874c37eff52986a6d6c691f5
Thiago Sampaio <thiagosampaioparticular1@gmail.com>
2022-01-03 15:32:10 -0300

    Organiza comentários da aula 1 e adicona aula 2

--- exercicios-js/1_fundamentos/1_organizacao.js
+++ exercicios-js/1_fundamentos/1_organizacao.js
@@ -1,3 +1,11 @@
+/* 
+ * Como o javaScript é organizado?
+ *
+ * O Js é organizado através de SENTENÇAS DE CÓDIGO, podendo terminar ou não, em ponto e virgula.
+ * E tbm é organizado em BLOCOS DE CÓDIGO, delimitados por chaves , sem possuir hierarquia de indentação * como python.
+*/
+
+
 console.log("Sentença de código")
 
 {
@@ -9,3 +17,4 @@ console.log("Sentença de código")
 
 // 3 Sentenças de código
 // 2 blocos de código
+
