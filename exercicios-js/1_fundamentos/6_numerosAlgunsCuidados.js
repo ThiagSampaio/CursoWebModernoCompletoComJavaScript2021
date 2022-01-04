d0b0e8084be137e7292ed08a1cbd1b34fa07601c
Thiago Sampaio <thiagosampaioparticular1@gmail.com>
2022-01-04 08:21:52 -0300

    Implemanta aula 6

---
+++ exercicios-js/1_fundamentos/6_numerosAlgunsCuidados.js
@@ -0,0 +1,10 @@
+// Vamos mostrar algumas operações estranhas 
+
+console.log(7 / 0) // tipo Infinity
+
+console.log('10' / 2) // linguagem fracamente tipada 
+console.log("Show" * 2)
+console.log(0.1 + 0.7) // pela especificação gera uma impresição , para ser mais rápido
+//console.log(10.toString())// consegue fazer apenas em um number 
+console.log((10.345).toFixed(2))
+console.log((10.345).toString())
